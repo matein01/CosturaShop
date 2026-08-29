fb1c78fae503aee744375c8790110db67ab9803b
Mateo <mateo.guerrero.or@gmail.com>
2026-08-29 00:06:43 -0500

    Primera migracion aplicada: tablas Productos, Combos, Pedidos, DetallesPedidos con restricciones de cascada configuradas

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -17,5 +17,16 @@ namespace CosturaShop.Data
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Producto> Productos { get; set; }
 
+      protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Producto>().Property(x => x.Precio).HasPrecision(10, 2);
+
+      modelBuilder.Entity<Combo>().Property(x => x.Precio).HasPrecision(10, 2);
+
+      modelBuilder.Entity<Combo>().HasOne(c => c.Pantalon).WithMany().HasForeignKey(c => c.PantalonId).OnDelete(DeleteBehavior.Restrict);
+
+      modelBuilder.Entity<Combo>().HasOne(c => c.Chaqueta).WithMany().HasForeignKey(c => c.ChaquetaId).OnDelete(DeleteBehavior.Cascade);
+    }
+
     }
 }
---
+++ Migrations/20260829050323_InicialCreacionTablas.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CosturaShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class InicialCreacionTablas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NombreCliente = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Precio = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Material = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Talla = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    ImagenUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Combos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Precio = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    ChaquetaId = table.Column<int>(type: "int", nullable: false),
+                    PantalonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Combos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Combos_Productos_ChaquetaId",
+                        column: x => x.ChaquetaId,
+                        principalTable: "Productos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Combos_Productos_PantalonId",
+                        column: x => x.PantalonId,
+                        principalTable: "Productos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetallesPedidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PedidoId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    ProductoId = table.Column<int>(type: "int", nullable: true),
+                    ComboId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetallesPedidos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetallesPedidos_Combos_ComboId",
+                        column: x => x.ComboId,
+                        principalTable: "Combos",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_DetallesPedidos_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DetallesPedidos_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Combos_ChaquetaId",
+                table: "Combos",
+                column: "ChaquetaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Combos_PantalonId",
+                table: "Combos",
+                column: "PantalonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetallesPedidos_ComboId",
+                table: "DetallesPedidos",
+                column: "ComboId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetallesPedidos_PedidoId",
+                table: "DetallesPedidos",
+                column: "PedidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetallesPedidos_ProductoId",
+                table: "DetallesPedidos",
+                column: "ProductoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetallesPedidos");
+
+            migrationBuilder.DropTable(
+                name: "Combos");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
