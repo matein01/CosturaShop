a09bca9623c07614d4e887350f1b818173d879af
Mateo <mateo.guerrero.or@gmail.com>
2026-08-30 01:30:31 -0500

    CRUD Productos: acciones Index y Create (GET/POST) con sus vistas

---
+++ Controllers/ProductosController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CosturaShop.Models;
+using CosturaShop.Data;
+
+namespace CosturaShop.Controllers
+{
+    public class ProductosController : Controller
+  {
+    private readonly ApplicationDbContext _dbContext;
+
+    public ProductosController(ApplicationDbContext Context)
+    {
+      this._dbContext = Context;
+    }
+
+    public IActionResult Index()
+    {
+      var productos = _dbContext.Productos.ToList();
+
+      return View(productos);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Producto producto)
+    {
+      _dbContext.Productos.Add(producto);
+      _dbContext.SaveChanges();
+
+      return RedirectToAction("Index");
+    }
+  }
+}
