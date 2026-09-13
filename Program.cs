0e3d6e275841b6bee49588caa0765a78a4872a46
Mateo <mateo.guerrero.or@gmail.com>
2026-09-12 19:26:44 -0500

    Simplificacion de reglas de password y creacion automatica del usuario administrador al iniciar

--- Program.cs
+++ Program.cs
@@ -9,16 +9,41 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+  options.Password.RequireDigit = false;
+  options.Password.RequireLowercase = false;
+  options.Password.RequireUppercase = false;
+  options.Password.RequireNonAlphanumeric = false;
+  options.Password.RequiredLength = 6;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+  var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+  var saber = await userManager.FindByEmailAsync("correo_prueba@gmail.com");
+
+  if (saber == null)
+  {
+    var user = new IdentityUser
+    {
+      UserName = "correo_prueba@gmail.com",
+      Email = "correo_prueba@gmail.com"
+    };
+
+    await userManager.CreateAsync(user, "123456");
+  }
 }
 
 app.UseHttpsRedirection();
