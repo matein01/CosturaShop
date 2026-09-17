765dc13381348da84ea1b5e2a5ee6b996cea6cc0
Mateo <mateo.guerrero.or@gmail.com>
2026-09-16 19:10:07 -0500

    CRUD de Combos completo: Edit y Delete (GET/POST), con Include() para cargar Chaqueta/Pantalon

--- Controllers/CombosController.cs
+++ Controllers/CombosController.cs
@@ -50,6 +50,60 @@ namespace CosturaShop.Controllers
 
       return RedirectToAction("Index");
     }
+
+    [HttpGet]
+    public IActionResult Edit(int idComboEditar)
+    {
+      var chaquetas = _dbContext.Productos.Where(p => p.Tipo == TipoProducto.Chaqueta);
+      var pantalones = _dbContext.Productos.Where(p => p.Tipo == TipoProducto.Pantalon);
+
+      var listaChaquetas = new SelectList(chaquetas, "Id", "Nombre");
+      var listaPantalones = new SelectList(pantalones, "Id", "Nombre");
+
+      ViewBag.Chaquetas = listaChaquetas;
+      ViewBag.Pantalones = listaPantalones;
+
+      var comboEditar = _dbContext.Combos.Find(idComboEditar);
+
+      return View(comboEditar);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Combo combo)
+    {
+      combo.Chaqueta = null;
+      combo.Pantalon = null;
+
+      _dbContext.Combos.Update(combo);
+      _dbContext.SaveChanges();
+
+      return RedirectToAction("Index");
+    }
     
+    [HttpGet]
+    public IActionResult Delete(int idComboEliminar)
+    {
+      var comboEliminar = _dbContext.Combos.Include(c => c.Chaqueta).Include(c => c.Pantalon).FirstOrDefault(c => c.Id == idComboEliminar);
+
+      return View(comboEliminar);
+    }
+
+    [HttpPost]
+    [ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int Id)
+    {
+      var comboEliminar = _dbContext.Combos.Include(c => c.Chaqueta).Include(c => c.Pantalon).FirstOrDefault(c => c.Id == Id);
+      if (comboEliminar == null)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        _dbContext.Combos.Remove(comboEliminar);
+        _dbContext.SaveChanges();
+
+        return RedirectToAction("Index");
+      }
+    }
   }
 }
